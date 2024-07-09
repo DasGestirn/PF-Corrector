c72352cead2ae9477975386b5efb3cd5c5dd53c5
DasGestirn <schwarze@tutanota.com>
2024-07-08 22:55:29 -0300

    Changes to footer, vite.config and README files

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 const root = resolve(__dirname, 'src');
-const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
     root,
     build: {
+        emptyOutDir: true,
+        outDir:"/dist",
         rollupOptions: {
             input: {
                 complete: resolve(root, 'index.html'),
